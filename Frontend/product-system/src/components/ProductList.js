2cc0d1523b3b0eecc3e02e0921bac0a09d8909d3
furkan1499 <furkan14991499@gmail.com>
2022-02-26 15:08:16 +0300

    add create modal test

--- Frontend/product-system/src/components/ProductList.js
+++ Frontend/product-system/src/components/ProductList.js
@@ -7,9 +7,26 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Button, Modal, Box, Typography } from '@mui/material';
 
 const ProductList = (props) => {
     const [productList, setProductList] = useState([]);
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    };
 
     useEffect(() => {
         axios
@@ -22,6 +39,24 @@ const ProductList = (props) => {
 
     return (
         <TableContainer component={Paper}>
+            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <Button onClick={handleOpen} variant="contained">Create</Button>
+            </div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Test
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        test
+                    </Typography>
+                </Box>
+            </Modal>
             <Table aria-label="simple table">
                 <TableHead>
                     <TableRow>
